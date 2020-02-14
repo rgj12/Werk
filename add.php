68ebcceb2ff6278fea50dddf97c1c2466aebc6ec
renato <renatogomes600@gmail.com>
2020-02-14 09:51:40 +0100

    input bij klanten toevoegen gevalideerd

--- add.php
+++ add.php
@@ -7,23 +7,36 @@ $afspraken = new Afspraken;
 
 //klanten toevoegen
 if (isset($_POST['toevoegen'])) {
+
     $data = array();
 
-    $data['vnaam'] = $_POST['voornaam'];
-    $data['anaam'] = $_POST['achternaam'];
-    $data['mail'] = $_POST['email'];
-    $data['stnaam'] = $_POST['straatnaam'];
-    $data['pcode'] = $_POST['postcode'];
-    $data['wplaats'] = $_POST['woonplaats'];
-    $data['tel'] = $_POST['telefoonnummer'];
-    $data['reden'] = $_POST['reden'];
+    //valideer input
+    if (!empty($_POST['voornaam']) || !empty($_POST['achternaam']) || !empty($_POST['email']) || !empty($_POST['straatnaam']) || !empty($_POST['postcode']) || !empty($_POST['woonplaats']) || !empty($_POST['telefoonnummer']) || !empty($_POST['reden'])) {
 
-    if ($klanten->addCustomer($data)) {
-        redirect('klanten.php', 'Succesvol toegevoegd', 'success');
+        $data['vnaam'] = trim(htmlspecialchars($_POST['voornaam']));
+        $data['anaam'] = trim(htmlspecialchars($_POST['achternaam']));
+        $data['mail'] = $_POST['email'];
+        $data['stnaam'] = trim(htmlspecialchars($_POST['straatnaam']));
+        $data['pcode'] = $_POST['postcode'];
+        $data['wplaats'] = trim(htmlspecialchars($_POST['woonplaats']));
+        $data['tel'] = $_POST['telefoonnummer'];
+        $data['reden'] = trim(htmlspecialchars($_POST['reden']));
+
+        //valideer email
+        $data['mail'] = filter_var($data['mail'], FILTER_VALIDATE_EMAIL);
+
+        if ($data['mail'] == false) {
+            redirect('klanten.php', 'Vul een juiste email in', 'error');
+        }
+
+        if ($klanten->addCustomer($data)) {
+            redirect('klanten.php', 'Succesvol toegevoegd', 'success');
+        } else {
+            redirect('klanten.php', 'Er is iets misgegaan', 'error');
+        }
     } else {
-        redirect('klanten.php', 'Er is iets misgegaan', 'danger');
+        redirect('klanten.php', 'Vul alle velden in!', 'error');
     }
-
 }
 
 //maak afspraak voor klant
