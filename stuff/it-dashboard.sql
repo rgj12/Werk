5ff3109d0d2862f51f3d59f751b7c88d7c998c2b
renato <renatogomes600@gmail.com>
2020-02-26 10:27:33 +0100

    database aangepast

--- stuff/it-dashboard.sql
+++ stuff/it-dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 21 feb 2020 om 12:07
+-- Gegenereerd op: 26 feb 2020 om 10:25
 -- Serverversie: 10.4.11-MariaDB
 -- PHP-versie: 7.4.1
 
@@ -34,7 +34,7 @@ CREATE TABLE `afspraken` (
   `datum` date NOT NULL,
   `tijd` time NOT NULL,
   `omschrijving` varchar(255) NOT NULL,
-  `datum_afspr_gemaakt` datetime NOT NULL DEFAULT current_timestamp(),
+  `datum_afspr_gemaakt` date NOT NULL DEFAULT current_timestamp(),
   `afspraak_voltooid` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -43,7 +43,11 @@ CREATE TABLE `afspraken` (
 --
 
 INSERT INTO `afspraken` (`id`, `klant_id`, `datum`, `tijd`, `omschrijving`, `datum_afspr_gemaakt`, `afspraak_voltooid`) VALUES
-(32, 51, '2020-02-27', '12:00:00', 'ddfdsfds', '2020-02-18 10:12:12', 1);
+(39, 2, '2020-02-26', '12:20:00', '', '2020-02-25', 1),
+(40, 2, '2020-02-25', '12:12:00', 'test', '2020-02-25', 1),
+(41, 2, '2020-02-25', '12:12:00', 'tesdt', '2020-02-25', 1),
+(42, 2, '2020-02-25', '12:12:00', 'th', '2020-02-25', 0),
+(43, 2, '2020-02-20', '12:42:00', 'test', '2020-02-25', 0);
 
 -- --------------------------------------------------------
 
@@ -69,7 +73,8 @@ INSERT INTO `chat_message` (`chat_message_id`, `to_user_id`, `from_user_id`, `ch
 (6, 2, 1, 'testmessage', '2020-02-21 10:14:10'),
 (7, 2, 1, 'test', '2020-02-21 11:52:14'),
 (8, 1, 2, 'sdsd', '2020-02-21 12:01:03'),
-(9, 1, 2, 'derp', '2020-02-21 12:01:20');
+(9, 1, 2, 'derp', '2020-02-21 12:01:20'),
+(10, 1, 2, 'derpx2', '2020-02-21 12:49:02');
 
 -- --------------------------------------------------------
 
@@ -80,7 +85,7 @@ INSERT INTO `chat_message` (`chat_message_id`, `to_user_id`, `from_user_id`, `ch
 CREATE TABLE `diensten` (
   `id` int(11) NOT NULL,
   `dienstnaam` varchar(255) NOT NULL,
-  `dienstprijs` int(11) NOT NULL
+  `dienstprijs` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -88,7 +93,8 @@ CREATE TABLE `diensten` (
 --
 
 INSERT INTO `diensten` (`id`, `dienstnaam`, `dienstprijs`) VALUES
-(1, 'testdiensten', 25);
+(1, 'testdiensten', '7.50'),
+(5, 'opschonen', '125.00');
 
 -- --------------------------------------------------------
 
@@ -100,6 +106,11 @@ CREATE TABLE `facturen` (
   `factuurnummer` int(11) NOT NULL,
   `voornaam` varchar(255) NOT NULL,
   `achternaam` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `straatnaam` varchar(255) NOT NULL,
+  `postcode` varchar(255) NOT NULL,
+  `woonplaats` varchar(255) NOT NULL,
+  `telefoonnummer` varchar(255) NOT NULL,
   `klantnummer` int(11) NOT NULL,
   `product1` varchar(255) NOT NULL,
   `product2` varchar(255) NOT NULL,
@@ -114,10 +125,20 @@ CREATE TABLE `facturen` (
   `dienst_prijs2` varchar(45) NOT NULL,
   `dienst_prijs3` varchar(45) NOT NULL,
   `totaalIncBtw` varchar(255) NOT NULL,
-  `totaalExBtw` varchar(45) NOT NULL,
+  `totaalExBtw` varchar(255) NOT NULL,
+  `totaalBTW` varchar(255) NOT NULL,
   `datum` date NOT NULL,
   `betaalOpties` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `facturen`
+--
+
+INSERT INTO `facturen` (`factuurnummer`, `voornaam`, `achternaam`, `email`, `straatnaam`, `postcode`, `woonplaats`, `telefoonnummer`, `klantnummer`, `product1`, `product2`, `product3`, `dienst1`, `dienst2`, `dienst3`, `product_prijs1`, `product_prijs2`, `product_prijs3`, `dienst_prijs1`, `dienst_prijs2`, `dienst_prijs3`, `totaalIncBtw`, `totaalExBtw`, `totaalBTW`, `datum`, `betaalOpties`) VALUES
+(9, 'Gerritax', 'van de waalx', 'w@wx.nl', 'koekstraatx', '3192JWx', 'Rotterdamx', '', 2, 'tesprod3 ', ' ', ' ', 'opschonen ', ' ', ' ', ' 8.50', ' 0', ' 0', ' 125.00', ' 0', ' 0', '133.50', '110.33', '23.17', '2020-02-25', '14 dagen'),
+(10, 'Gerrit', 'van de waal', 'w@w.nl', 'koekstraat', '3192JW', 'Rotterdam', '064559621', 2, 'harde schijf ', 'opvouwbare kut ', ' ', ' ', ' ', ' ', ' 25.00', ' 25.95', ' 0', ' 0', ' 0', ' 0', '50.95', '42.11', '8.84', '2020-02-26', 'pin'),
+(11, 'Gerrit', 'van de waal', 'w@w.nl', 'koekstraat', '3192JW', 'Rotterdam', '064559621', 2, 'pc ', 'pc ', ' ', ' ', ' ', ' ', ' 424.68', ' 424.68', ' 0', ' 0', ' 0', ' 0', '849.36', '701.95', '147.41', '2020-02-26', 'IDEAL');
 
 -- --------------------------------------------------------
 
@@ -142,9 +163,7 @@ CREATE TABLE `klanten` (
 --
 
 INSERT INTO `klanten` (`id`, `voornaam`, `achternaam`, `email`, `straatnaam`, `postcode`, `woonplaats`, `telefoonnummer`, `reden_bezoek`) VALUES
-(51, 'Heerikhghaaaaa', 'Heerik', 'dvdheerik@hetnet.nl', '', '', 'Alphen', '0656897412', 'tedt'),
-(52, 'Nieuwehuizen', 'Heerik', 'dvdheerik@hetnet.nl', 'Heerik', 'Heerik', 'Heerik', 'Heerik', 'Heerik'),
-(53, 'test`123', 'test', 'test@test.nl', '', '', 'Rhoon', '12345678', 'sdadff');
+(2, 'Gerrit', 'van de waal', 'w@w.nl', 'koekstraat', '3192JW', 'Rotterdam', '064559621', 'laptop inlveren om gerepareerd');
 
 -- --------------------------------------------------------
 
@@ -175,8 +194,10 @@ CREATE TABLE `producten` (
 --
 
 INSERT INTO `producten` (`id`, `productnaam`, `prijs`) VALUES
-(1, 'testprod', '12.00'),
-(7, 'testproduct2', '45.00');
+(7, 'pc', '424.68'),
+(8, 'tesprod3', '8.50'),
+(9, 'harde schijf', '25.00'),
+(10, 'opvouwbare kut', '25.95');
 
 -- --------------------------------------------------------
 
@@ -260,31 +281,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `afspraken`
 --
 ALTER TABLE `afspraken`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT voor een tabel `chat_message`
 --
 ALTER TABLE `chat_message`
-  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `diensten`
 --
 ALTER TABLE `diensten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT voor een tabel `facturen`
 --
 ALTER TABLE `facturen`
-  MODIFY `factuurnummer` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `factuurnummer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT voor een tabel `klanten`
 --
 ALTER TABLE `klanten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT voor een tabel `login_details`
@@ -296,7 +317,7 @@ ALTER TABLE `login_details`
 -- AUTO_INCREMENT voor een tabel `producten`
 --
 ALTER TABLE `producten`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
